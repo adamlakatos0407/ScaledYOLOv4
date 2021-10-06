03ee94b4c971ba64d9fc5e36cebd3b008cf21204
adamlakatos0407 <65445171+adamlakatos0407@users.noreply.github.com>
2021-10-06 14:08:40 +0200

    Update train.py

--- train.py
+++ train.py
@@ -343,7 +343,7 @@ def train(hyp, opt, device, tb_writer=None):
 
                 # Save last, best and delete
                 torch.save(ckpt, last)
-                if epoch >= (epochs-30):
+                if epoch >= (epochs-30) and (epoch % 10 == 0):
                     torch.save(ckpt, last.replace('.pt','_{:03d}.pt'.format(epoch)))
                 if best_fitness == fi:
                     torch.save(ckpt, best)
